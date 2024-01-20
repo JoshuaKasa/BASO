1721de9b35a0e79eb3947e9109a41ec28532b3f6
Joshua | Tommaso <87675824+JoshuaKasa@users.noreply.github.com>
2024-01-20 01:52:41 +0100

    I dont know what the fuck this is

---
+++ corel/target/debug/deps/corel-07b0d62d6db03af6.d
@@ -0,0 +1,7 @@
+C:\Users\jizos\Documents\Programming\Python\BASO\corel\target\debug\deps\libcorel-07b0d62d6db03af6.rmeta: src\main.rs src\corel_lexer.rs src\corel_parser.rs
+
+C:\Users\jizos\Documents\Programming\Python\BASO\corel\target\debug\deps\corel-07b0d62d6db03af6.d: src\main.rs src\corel_lexer.rs src\corel_parser.rs
+
+src\main.rs:
+src\corel_lexer.rs:
+src\corel_parser.rs:
